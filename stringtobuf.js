6664df44b361ca9c943449aa1ad7b40d7444a5d6
ghostsnstuff <jaredhalpert90@gmail.com>
2014-03-03 23:25:40 -0800

    Update stringtobuf.js

--- stringtobuf.js
+++ stringtobuf.js
@@ -1,5 +1,5 @@
 /*
-For this challenge, write a node program that prints a buffer object containing
+for this challenge, write a node program that prints a buffer object containing
 the string "bytewiser" using console.log.
 */
 console.log(new Buffer('bytewiser'));
